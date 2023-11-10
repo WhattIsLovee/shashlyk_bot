2f51c5f7c68a8368fce5deb3ff910aaa074d14e4
Vladislav <shavayev_vladislav@list.ru>
2023-11-10 19:35:58 +0200

    attempt to create a keyboard.

--- keyboard.py
+++ keyboard.py
@@ -3,8 +3,8 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
 from constants import PICKLE_GUIDE
 
 
-button_pickle_guide = KeyboardButton(PICKLE_GUIDE)
+#button_pickle_guide = KeyboardButton(PICKLE_GUIDE)
 
-main_kb = ReplyKeyboardMarkup()
-main_kb.add(button_pickle_guide)
+#main_kb = ReplyKeyboardMarkup()
+#main_kb.add(button_pickle_guide)
 
