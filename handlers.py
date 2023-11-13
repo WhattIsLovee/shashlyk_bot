3b5c9779c62b132d292e3702bd0ee9c808faf0e3
Vladislav <shavayev_vladislav@list.ru>
2023-11-13 23:05:23 +0200

    attempt to update a keyboard.

--- handlers.py
+++ handlers.py
@@ -4,6 +4,7 @@ from aiogram.filters import Command
 from aiogram import Dispatcher
 
 import keyboard as kb
+from keyboard import create_keyboard
 
 import constants
 
@@ -17,6 +18,7 @@ async def start_handler(msg: Message):
     await msg.answer('Я могу помочь Вам при решении шашлычных вопросов.', reply_markup=kb.menu_kb)
     #await msg.answer(MENU, reply_markup=kb.exit_kb)
 
+
 @router.message(F.text == 'Галерея')
 async def galery_handler(msg: Message):
     await msg.answer(constants.GALLERY, reply_markup=kb.gallery_kb)
@@ -25,3 +27,8 @@ async def galery_handler(msg: Message):
 @router.message(F.text == "Меню")
 async def menu(msg: Message):
     await msg.answer(constants.MENU, reply_markup=kb.menu_kb)
+
+
+@router.message(F.text == "1")
+async def button1_handler(msg: Message):
+    await msg.answer("Вы нажали на Кнопку 1", reply_markup=create_keyboard)
