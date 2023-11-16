0f5148e028bd899f773046e9d0cc89c8c9a484e4
Vladislav <shavayev_vladislav@list.ru>
2023-11-16 23:38:08 +0200

    ...

--- keyboard.py
+++ keyboard.py
@@ -1,15 +1,7 @@
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-from constants import PICKLE_GUIDE
-
 
 menu_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = 'Меню')]], resize_keyboard=True)
 
 gallery_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = 'Галерея')]], resize_keyboard=True)
 
-def create_keyboard():
-    kb1 = ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = KeyboardButton("Кнопка 1")
-    button2 = KeyboardButton("Кнопка 2")
-    kb1.row(button1, button2)
-    return kb1
