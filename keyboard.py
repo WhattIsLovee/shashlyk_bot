e39385cc6e199bd60529dca8ebac9cec729c9437
Vladislav <shavayev_vladislav@list.ru>
2023-11-21 22:28:50 +0200

    ...

--- keyboard.py
+++ keyboard.py
@@ -4,14 +4,3 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
 menu_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = 'Меню')]], resize_keyboard=True)
 
 gallery_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = 'Галерея')]], resize_keyboard=True)
-
-class MyKeyboard:
-    @staticmethod
-    def get_keyboard():
-        button1 = KeyboardButton('Button 1')
-        button2 = KeyboardButton('Button 2')
-        
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(button1, button2)
-        
-        return keyboard
