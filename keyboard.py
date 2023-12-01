5b47316785dba3a441529bfdb2f6aa93ae346807
Vladislav <shavayev_vladislav@list.ru>
2023-12-01 20:39:43 +0200

    marinade keyboard improvement.

--- keyboard.py
+++ keyboard.py
@@ -1,12 +1,17 @@
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-from constants import FRYING_GUIDE, PICKLE_GUIDE, GALLERY
+from constants import FRYING_GUIDE, PICKLE_GUIDE, GALLERY, MENU, BACK, FAVORITES
 
 
 menu_kb = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text = FRYING_GUIDE)],
     [KeyboardButton(text = PICKLE_GUIDE)],
+    [KeyboardButton(text = FRYING_GUIDE)],
     [KeyboardButton(text = GALLERY)],
+    ], resize_keyboard=True)
+
+pickle_kb = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text = '🫙 Маринады'), KeyboardButton(text = '📒 Мои рецепты')],
+    [KeyboardButton(text = FAVORITES), KeyboardButton(text = MENU)],
     ], resize_keyboard=True)
 
 gallery_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = 'Галерея')]], resize_keyboard=True)
